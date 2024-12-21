5829c36762c9eaba842d3e686ce7b82608516433
zeroni002 <leonorzgg@gmail.com>
2024-12-22 06:29:01 +0700

    update signup

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  signupForm: FormGroup;
 
+  constructor() {
+    this.signupForm = this.createFormGroup();
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  createFormGroup(): FormGroup {
+    return new FormGroup(
+      {
+        name: new FormControl("", [Validators.required, Validators.minLength(2)]),
+        email: new FormControl("", [Validators.required, Validators.email]),
+        password: new FormControl("", [Validators.required, Validators.minLength(7)]),
+      }
+    )
+  }
+
+  signup(): void {
+    console.log(this.signupForm.value);
+  }
 }
